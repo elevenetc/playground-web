83944ffec0b5ef086c9bb2ddd704060b7afa5d31
Eugene Levenetc <shumvlesu@gmail.com>
2016-06-10 23:27:13 +0200

    Update GroundModel tests

---
+++ js/candc/tests/TestGroundModel.js
@@ -0,0 +1,65 @@
+var Composite = require('../components/Composite');
+var GroundModel = require('../components/GroundModel');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+class TestGroundModel {
+
+	constructor() {
+		this.matrix = null;
+		/** @type {GroundModel} */
+		this.groundModel = null;
+	}
+
+	before() {
+		this.matrix = [
+			[0, 0, 0],
+			[0, 0, 0],
+			[0, 0, 0]
+		];
+		this.groundModel = new GroundModel(this.matrix);
+	}
+
+	testPathFinding() {
+		this.before();
+		var x = 1;
+		var y = 2;
+		var path = this.groundModel.findPath(0, 0, x, y);
+		var lastPoint = path[path.length - 1];
+		path.should.not.equal(null);
+		lastPoint[1].should.equal(x);
+		lastPoint[0].should.equal(y);
+	}
+
+	testOccupation() {
+		this.before();
+		var x = 1;
+		var y = 2;
+		this.groundModel.occupy(null, x, y);
+		this.groundModel.isAvailable(x, y).should.equal(false);
+		this.groundModel.isAvailable(0, 0).should.equal(true);
+	}
+
+	testClear() {
+		this.before();
+		var x = 0;
+		var y = 0;
+		this.groundModel.isAvailable(x, y).should.equal(true);
+		this.groundModel.occupy(null, x, y);
+		this.groundModel.isAvailable(x, y).should.equal(false);
+		this.groundModel.clear(null, x, y);
+		this.groundModel.isAvailable(x, y).should.equal(true);
+	}
+
+
+	run() {
+		this.testPathFinding();
+		this.testOccupation();
+		this.testClear();
+	}
+}
+
+new TestGroundModel().run();
+
+
