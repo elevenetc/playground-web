55521c587a39e05d5d7028f969ad9a71e5f36f49
Eugene Levenetc <shumvlesu@gmail.com>
2016-06-12 16:12:14 +0200

    Add browserify sample

---
+++ js/sample-browserify/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by eleven on 12/06/2016.
+ */
+class App {
+	constructor() {
+		console.log('app: created');
+	}
+
+	init() {
+		console.log('app: init');
+	}
+}
+
+module.exports = App;
