655180c5f22af612d119ccf86141dad1557071d3
Eugene Levenetc <shumvlesu@gmail.com>
2016-06-26 23:52:47 +0200

    Minor logging refactoring

--- js/candc/log/Console.js
+++ js/candc/log/Console.js
@@ -4,19 +4,33 @@
 var WeightComponent = require('../components/WeightComponent');
 
 class con {
+
+	static logError(e) {
+		if (con.logMode >= con.LogModeError)
+			console.log(e);
+	}
+
 	static log(id, message) {
 
-		var idValue;
+		if (con.LogMode >= con.LogModeMessages) {
+			var idValue;
 
-		if (id.hasOwnProperty('getId')) idValue = id.getId();
-		else idValue = id;
+			if (id.hasOwnProperty('getId')) idValue = id.getId();
+			else idValue = id;
 
-		con.internalLog(idValue, message);
+			con.internalLog(idValue, message);
+		}
 	}
 
 	static internalLog(id, message) {
 		console.log(id + ': ' + message);
 	}
 }
+
+con.LogMode = undefined;
+con.LogModeAll = 10;
+con.LogModeError = 5;
+con.LogModeMessages = 2.5;
+con.LogModeNone = 0;
 
 module.exports = con;
